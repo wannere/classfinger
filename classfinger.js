8565fd7a4fbe771f39678aa36a4213dc4030715f
Emily Wanner <wannere@wwu.edu>
2019-01-17 13:13:59 -0800

    started results page, took out scratch sheet editable item, added to css and js files

--- classfinger.js
+++ classfinger.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $(".advanced_button").click(function(){
     $(".advanced_search").slideToggle().css("display", "flex");
   });
+  $(".advanced_button_small").click(function(){
+    $(".advanced_search").slideToggle().css("display", "flex");
+  });
 });
