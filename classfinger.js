9f3803caaf293723a3a7d19e9da70e65467c703b
Emily Wanner <wannere@wwu.edu>
2019-01-17 15:28:10 -0800

    added course summary slider, add to scratch button, copyright

--- classfinger.js
+++ classfinger.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
   $(".advanced_button_small").click(function(){
     $(".advanced_search").slideToggle().css("display", "flex");
   });
+  $(".desc").click(function(event){
+    $(this).siblings(".course_description").slideToggle().css("display", "flex");
+    event.stopPropagation();
+  });
 });
